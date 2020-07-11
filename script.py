d0a95b6a341f697da6765abd9d1576b32361dff0
Aden Eilers <adeneilers@gmail.com>
2020-07-11 14:08:09 -0700

    finished script for adding raw data to db

--- script.py
+++ script.py
@@ -7,9 +7,15 @@ from csv import DictReader
 #for each gameId in the kaggle csv itterate through them
 with open('Challenger_Ranked_Games.csv','r') as file:
   csvDictReader = DictReader(file)
+
+  returned504Times = 0
+
   for row in csvDictReader:
     gameId = row['gameId']
+    gameDuraton = row['gameDuraton']
 
+    if int(gameDuraton) < 900:
+      continue
     # api-endpoint 
     URL = "https://kr.api.riotgames.com/lol/match/v4/matches/" + gameId + "?api_key=RGAPI-1b4b5a24-db0d-4031-b75a-ce22428d6c9b"
 
@@ -18,9 +24,17 @@ with open('Challenger_Ranked_Games.csv','r') as file:
 
     # if the request encounters an error it will check if it is too many requests, if it is, then it will pause for 2 minutes and one second, other errors stop the program
     if r.status_code != 200:
-      if r.status_code == 429:
-        print("Error 429, sleeping program for 121 seconds")
-        time.sleep(121)
+      if r.status_code == 429 or r.status_code == 504:
+        returned504Times = returned504Times + 1
+        print("\nStatus 504, sleeping program for 1 seconds\n")
+        time.sleep(1)
+
+        if returned504Times > 2:
+          print("\n Status 504 3 times in a row, sleeping for 120 seconds \n")
+          returned504Times = 0
+          time.sleep(120)
+  
+        continue
       else:
         print("Error: " + str(r.status_code) + "returned by Riot API")
         sys.exit(0)
@@ -29,26 +43,26 @@ with open('Challenger_Ranked_Games.csv','r') as file:
     data = r.json() 
 
     #run this 10 times per request
-    def returnedData(data, value):
+    def returnedData(data, iterations):
 
-      if data["participants"][value]["teamId"] == 100:
+      if data["participants"][iterations]["teamId"] == 100:
         firstDragon = data["teams"][0]["firstDragon"]
       else:
         firstDragon = data["teams"][1]["firstDragon"]
 
       return {"gameId": data["gameId"], 
-              "championId": data["participants"][value]["championId"], 
-              "teamId": data["participants"][value]["teamId"], 
-              "damageDealt": data["participants"][value]["stats"]["totalDamageDealt"], 
-              "assists": data["participants"][value]["stats"]["assists"], 
-              "kills": data["participants"][value]["stats"]["kills"], 
-              "totalHealing": data["participants"][value]["stats"]["totalHeal"], 
-              "totalMinionsKilled": data["participants"][value]["stats"]["totalMinionsKilled"], 
-              "win": data["participants"][value]["stats"]["win"],
-              "firstBloodKill": data["participants"][value]["stats"]["firstBloodKill"],
+              "championId": data["participants"][iterations]["championId"], 
+              "teamId": data["participants"][iterations]["teamId"], 
+              "damageDealt": data["participants"][iterations]["stats"]["totalDamageDealt"], 
+              "assists": data["participants"][iterations]["stats"]["assists"], 
+              "kills": data["participants"][iterations]["stats"]["kills"], 
+              "totalHealing": data["participants"][iterations]["stats"]["totalHeal"], 
+              "totalMinionsKilled": data["participants"][iterations]["stats"]["totalMinionsKilled"], 
+              "win": data["participants"][iterations]["stats"]["win"],
+              "firstBloodKill": data["participants"][iterations]["stats"]["firstBloodKill"],
               "firstDragon": firstDragon,
-              "firstTowerKill": data["participants"][value]["stats"]["firstTowerKill"],
-              "visionScore": data["participants"][value]["stats"]["visionScore"]}
+              "firstTowerKill": data["participants"][iterations]["stats"]["firstTowerKill"],
+              "visionScore": data["participants"][iterations]["stats"]["visionScore"]}
 
     #call function 10 times per request, add each object to the array
     game = []
@@ -56,7 +70,7 @@ with open('Challenger_Ranked_Games.csv','r') as file:
       value = returnedData(data,i)
       game.append(value)
     
-    print(game)
+    print(game[0]["gameId"])
 
 #send this data to to database 
 
